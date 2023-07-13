0fdb2cd865214a66db23090f27673cf5c8328fa4
Platykin <igor.alves.pinto11@gmail.com>
2023-07-13 08:55:14 -0300

    Start of error handling

---
+++ C#/Chapter3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chapter3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+
+        public static void Calculate()
+        {
+            Console.Write("Please, enter the first number number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Now enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#/Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstMethod();
+            Console.WriteLine("Enter 2 float numbers: ");
+            string firstInput = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+            float firstNumber = float.Parse(firstInput);
+            float secondNumber = float.Parse(secondInput);
+            Console.WriteLine("The multiplications of {0} and {1} is {2}", firstNumber, secondNumber , MultiplyMethod(firstNumber, secondNumber));
+            string s = "HeY ThErE !";
+            s = LowUpper(s);
+            Console.WriteLine(s);
+            Count(s);
+        }
+
+
+        public static void FirstMethod() //good practice to use PascalCase on methods names and classes names
+        {
+            Console.WriteLine("Called by a method");
+        }
+
+        public static string LowUpper(string lowUpperString)
+        {
+            string temp1 = lowUpperString.ToLower();
+            string temp2 = lowUpperString.ToUpper();
+            string result = temp1 + temp2;
+            return result;
+        }
+
+
+        public static float MultiplyMethod(float num1, float num2)
+        { 
+            return (num1 * num2);
+        }
+
+        public static void CountCharacter(string count)
+        {
+            Console.WriteLine("The amount of characters is {0}", count.Length);
+        }
+    }
+}
