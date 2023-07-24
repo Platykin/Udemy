2eda28b656e639df6a43a1c511aa3a3e1c42f676
Platykin <igor.alves.pinto11@gmail.com>
2023-07-24 20:36:08 -0300

    Add for loops. It works exacly like c++

---
+++ C#/Chapter4/EnhancedIfStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EnhancedIfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -52;
+            string stateOfMatter;
+
+            if (temperature < 0)
+            {
+                stateOfMatter = "Solid";
+            }
+            else
+            {
+                stateOfMatter = "Liquid";
+            }
+
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            temperature += 104;
+
+            //shorter way:
+            stateOfMatter = temperature < 0 ? "Solid" : "Liquid";
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+
+            //Unreadable way:
+            temperature += 52;
+            stateOfMatter = temperature > 100 ? "Gas" : temperature < 0 ? "Solid" : "Liquid";
+            Console.WriteLine("State of matter is {0}", stateOfMatter);
+        }
+    }
+}
---
+++ C#/Chapter5/For Loop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter < 20 ; counter+=2)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
