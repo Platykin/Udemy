bd4a928d0a1373390ebffcd11d82e35a5f30c062
Platykin <igor.alves.pinto11@gmail.com>
2023-08-08 19:19:22 -0300

    while loops with diagnostics

---
+++ C#/Chapter5/Do While Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics;
+
+namespace Do_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter < 5);
+            timer.Stop();
+
+            long timerMS = timer.ElapsedMilliseconds;
+            Console.WriteLine("O tempo percorrido foi de {0}ms", timerMS);
+
+        }
+    }
+}
