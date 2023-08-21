7bdf52c27c9cf35b97904a8788e3750697f9e7fc
Platykin <igor.alves.pinto11@gmail.com>
2023-08-21 19:12:04 -0300

    OOP beggining

---
+++ C#/Chapter6/Classes and Objects/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects
+{
+    internal class Car
+    {
+        private string _name;
+        private int _hp;
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            Console.WriteLine("{0} was created!", _name);
+            _hp = hp;
+        }
+
+        public void Drive() { Console.WriteLine("{0} is driving!", _name); }
+
+        public void Stop() { Console.WriteLine("{0} stoped!", _name); }
+
+        public void GetInfo() { Console.WriteLine("Model: {0}, Horse Power: {1}", _name, _hp); }
+
+    }
+}
---
+++ C#/Chapter6/Classes and Objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car audi = new Car("audi", 198);
+            audi.Drive();
+            
+            Car bmw = new Car("bmw", 120);
+
+            string userInput = "";
+            userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                audi.Stop();
+                bmw.Stop();
+            }
+            else 
+            {
+                audi.Drive();
+            }
+            bmw.GetInfo();
+            audi.GetInfo();
+        }
+    }
+}
