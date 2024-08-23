6d77c6b07af0ad9d9c13f39c3fdb51703085004e
Platykin <igor.alves.pinto11@gmail.com>
2024-08-23 10:21:29 -0300

    Make first project with tkinter, miles to Kilometers

---
+++ Tkinter/Project 1/Mile to Kilometer.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+    mile_input = entry_int.get()
+    kilometer_output = mile_input * 1.60934
+    output_string.set(kilometer_output)
+
+# window
+window = ttk.Window(themename = "darkly")
+window.title("Demo")
+window.geometry('300x150')
+
+# title
+title_label = ttk.Label(master = window, text = 'Miles to Kilometer', font = 'Calibri 24 bold')
+title_label.pack()
+
+# input
+input_frame = ttk.Frame(master = window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master = input_frame, textvariable= entry_int)
+button = ttk.Button(master = input_frame, text = "Convert", command = convert)
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+# output
+output_string = tk.StringVar()
+output_label = tk.Label(master = window, text = "Output",  font = 'Calibri 24 bold', textvariable= output_string)
+output_label.pack(pady = 5)
+
+# run
+window.mainloop()
