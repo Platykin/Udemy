682dd627de9601098bf28b2a5bb8fca0eeaa575d
Platykin <igor.alves.pinto11@gmail.com>
2024-08-26 09:33:38 -0300

    Add 4th lesson

---
+++ Tkinter/Project 4/GUI4.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+window = tk.Tk()
+window.title("Tkinter Variables")
+
+# Tkinter Variables
+string_variable = tk.StringVar(value = "test")
+
+# Widgets
+entry1 = ttk.Entry(master = window, textvariable = string_variable)
+entry1.pack()
+entry2 = ttk.Entry(master = window, textvariable = string_variable)
+entry2.pack()
+label = ttk.Label(master = window, textvariable = string_variable)
+label.pack()
+
+# Run
+window.mainloop()
